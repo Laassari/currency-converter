e49784508f7a581bb2608333e0322688d63e9659
Laassari <nlaassari@gmail.com>
2018-07-01 04:26:14 +0100

    add search for currency functionality
        bug fixes

--- scripts/app.js
+++ scripts/app.js
@@ -22,13 +22,33 @@ addEventListener('DOMContentLoaded', () => {
   firstCurrenciesList.addEventListener('click', listClicked)
   secondCurrenciesList.addEventListener('click', listClicked)
 
-
+  //filter lists when searched
+  firstCurrencySelect.addEventListener('input', function(){
+    renderMatches(this.value, firstCurrenciesList)
+  })
+  secondCurrencySelect.addEventListener('input', function(){
+    renderMatches(this.value, secondCurrenciesList)
+  })
 
   //hide lists when clicked outside of them
-  document.addEventListener('click', () => {
+  document.addEventListener('click', (event) => {
+    if (event.target.closest('.select-container')) return //ignore when clicked inside a select box
     firstCurrenciesList.classList.remove('show')
     secondCurrenciesList.classList.remove('show')
+
+    ;[firstCurrencyHolder, secondCurrencyHolder].forEach(elem => elem.classList.remove('hide'))
   })
+
+  function showList(event) {
+    event.stopPropagation()
+    this.previousElementSibling.focus()
+
+    ;[firstCurrenciesList, secondCurrenciesList] //only one list can show at a time
+      .filter(el => (el != this.nextElementSibling))[0].classList.remove('show')
+
+    this.classList.add('hide') //hide the div that holds the current currency
+    this.nextElementSibling.classList.add('show') //show the list
+  }
 })
 
 function renderElement(node, content, isInput) {
@@ -40,19 +60,15 @@ function renderElement(node, content, isInput) {
 }
 
 function listClicked(event) {
+  if (event.target.nodeName === 'UL') return //cliked item isn't a list
   event.stopPropagation()
   const currencyHolder = this.previousElementSibling
-  renderElement(currencyHolder, event.target.innerHTML, false)
+  renderElement(currencyHolder, event.target.closest('li').innerHTML, false)
   currencyHolder.classList.remove('hide')
   this.classList.remove('show')
 }
 
-function showList(event) {
-  event.stopPropagation()
-  console.log(this)
-  this.classList.add('hide') //hide the div that holds the current currency
-  this.nextElementSibling.classList.add('show') //show the list
-}
+
 
 async function getCurrenciesList() {
   const blob = await fetch('https://free.currencyconverterapi.com/api/v5/currencies')
@@ -84,5 +100,30 @@ function populateCurrenciesLists() {
           ulElement.appendChild(li.cloneNode(true))
         })
       })
+  })
+}
+
+
+async function renderMatches(value, destNode) {
+  const currenciesList = await getCurrenciesList()
+  destNode.classList.add('show')
+
+  const matches = Object.values(currenciesList.results).filter(result => { //remove non-matches from results
+    const regMatch = new RegExp(value, 'i')
+    return result.currencyName.match(regMatch) || result.id.match(regMatch)
+  })
+  
+  destNode.innerHTML = '' //empty the UL
+  matches.forEach(entry => {  //render matched results to UL
+
+    const li = document.createElement('li')
+    const img = document.createElement('img')
+    img.src = `../images/flags/${entry.id.toLowerCase()}.png`
+    const content = document.createTextNode(entry.currencyName)
+    li.appendChild(img)
+    li.appendChild(content)
+
+    
+    destNode.appendChild(li)
   })
 }
